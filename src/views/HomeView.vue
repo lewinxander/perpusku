e1e8fcd3463ca9cd3cdba5860b5f20e00f4adbb3
lewinxander <lewinxander@students.undip.ac.id>
2021-12-15 17:50:09 +0700

    persiapan awal

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent
+} from '@ionic/vue'
+</script>
+
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar color="primary">
+        <ion-title class="app-title">Perpusku</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content></ion-content>
+  </ion-page>
+</template>
+
+<style scoped>
+</style>
