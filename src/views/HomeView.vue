11a532bd5408f8abc979401372ce890bf1b4f162
lewinxander <lewinxander@students.undip.ac.id>
2021-12-16 14:58:52 +0700

    menambah tombol untuk segment daftar buku dan tambah buku

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,15 +4,23 @@ import {
   IonHeader,
   IonToolbar,
   IonTitle,
-  IonContent
+  IonContent,
+  IonSegment,
+  IonSegmentButton
 } from '@ionic/vue'
 </script>
 
 <template>
   <ion-page>
     <ion-header>
-      <ion-toolbar color="primary">
+      <ion-toolbar color="primary" mode="ios">
         <ion-title class="app-title">Perpusku</ion-title>
+      </ion-toolbar>
+      <ion-toolbar color="primary">
+        <ion-segment value="daftar">
+          <ion-segment-button value="daftar">Daftar Buku</ion-segment-button>
+          <ion-segment-button value="tambah">Tambah Buku</ion-segment-button>
+        </ion-segment>
       </ion-toolbar>
     </ion-header>
 
